6c76f0770926de22ba7b0e9308ca840fd21d2468
jmbrenne <jeremybrenner0@gmail.com>
2015-06-03 17:15:22 -0700

    Finished Solution

--- lib/race.rb
+++ lib/race.rb
@@ -1,5 +1,19 @@
 require_relative 'car'
 
 class Race
-  #write Race class code here
+	attr_accessor :cars, :winner, :loser
+  def initialize
+  	@cars = []
+  	2.times{@cars.push(Car.new)}
+  	@cars.each{|car| car.speed = rand(100)}
+  end
+
+  def winner
+  	@winner = @cars.first.speed > @cars.last.speed ? @cars[0] : @cars[1]
+  end
+
+  def loser
+  	@loser = @cars.first.speed < @cars.last.speed ? @cars[0] : @cars[1]
+  end
+
 end
